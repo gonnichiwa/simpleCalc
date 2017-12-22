f8ec3e1302367a0e45eae33556dabafff952d240
gonnichiwa <keepthefaith00@gmail.com>
2017-12-22 20:38:03 +0900

    함수제조

--- js/caculator.js
+++ js/caculator.js
@@ -1,15 +1,35 @@
-
-
+function plus(input1,input2){
+	var resultNumber = input1 + input2;
+	return resultNumber;
+}
 
 function caculate(){
+	/* 
+
+	*/
 	var inputTag1 = Number(document.getElementById('input1').value);
 	var inputTag2 = Number(document.getElementById('input2').value);
-
-	console.log("inputTag1 : " + inputTag1);
-	console.log("inputTag2 : " + inputTag2);
 	var target = document.getElementById('operators');
-
 	var op = target.options[target.selectedIndex].value;
+	var numOp = Number(op);
+	/*
 
-	console.log("operator : " + op);
+	*/
+	var result = 99999999999;
+	if(numOp == 1){
+		result = plus(inputTag1,inputTag2);
+	} else if (numOp == 2 ){
+		result = minus(inputTag1,inputTag2);
+		// result = inputTag1 - inputTag2;
+	} else if (numOp == 3){
+		result = multiple(inputTag1,inputTag2);
+		// result = inputTag1 * inputTag2;
+	} else if (numOp == 4){
+		result = division(inputTag1,inputTag2);
+		// result = inputTag1 / inputTag2;
+	}
+	/**
+
+	*/
+	document.getElementById('result').innerHTML = result;
 }
