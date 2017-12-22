41c37e589d90f50fee970d22b810349ac87dca52
gonnichiwa <keepthefaith00@gmail.com>
2017-12-22 18:57:31 +0900

    input tags 입력 js 콘솔로 데이터 출력 테스트

--- js/caculator.js
+++ js/caculator.js
@@ -2,5 +2,14 @@
 
 
 function caculate(){
-	alert('caculate!');
+	var inputTag1 = Number(document.getElementById('input1').value);
+	var inputTag2 = Number(document.getElementById('input2').value);
+
+	console.log("inputTag1 : " + inputTag1);
+	console.log("inputTag2 : " + inputTag2);
+	var target = document.getElementById('operators');
+
+	var op = target.options[target.selectedIndex].value;
+
+	console.log("operator : " + op);
 }
